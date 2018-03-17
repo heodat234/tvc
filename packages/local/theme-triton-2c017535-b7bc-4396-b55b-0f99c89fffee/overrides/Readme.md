421df8eb0ddd5e22d34f73f12045db023274ab10
heodat234 <thanhhung23495@gmail.com>
2018-03-17 14:08:56 +0700

    hung

---
+++ packages/local/theme-triton-2c017535-b7bc-4396-b55b-0f99c89fffee/overrides/Readme.md
@@ -0,0 +1,3 @@
+# theme-triton-2c017535-b7bc-4396-b55b-0f99c89fffee/overrides
+
+This folder contains overrides which will automatically be required by package users.
---
+++ packages/local/theme-triton-2c017535-b7bc-4396-b55b-0f99c89fffee/overrides/init.js
@@ -0,0 +1,2 @@
+Ext.namespace('Ext.theme.is')['theme-triton-2c017535-b7bc-4396-b55b-0f99c89fffee'] = true;
+Ext.theme.name = 'theme-triton-2c017535-b7bc-4396-b55b-0f99c89fffee';
